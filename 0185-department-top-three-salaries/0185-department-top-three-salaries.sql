320d0bef1d45ff1a9f5e6126d218237e30acf176
aspapriyan <aspapriyan2007@gmail.com>
2026-09-26 18:49:42 +0530

    Time: 964 ms (84.78%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,12 +1,22 @@
-SELECT d.name AS Department,
-       e.name AS Employee,
-              e.salary AS Salary
-              FROM Employee e
-              JOIN Department d
-              ON e.departmentId = d.id
-              WHERE 3 > (
-                  SELECT COUNT(DISTINCT e2.salary)
-                      FROM Employee e2
-                          WHERE e2.departmentId = e.departmentId
-                                AND e2.salary > e.salary
-                                );
+# Write your MySQL query statement below
+
+WITH new_table AS (
+    SELECT
+            d.name AS Department,
+                    e.name AS Employee,
+                            e.salary AS Salary,
+                                    DENSE_RANK() OVER (
+                                                PARTITION BY d.name
+                                                            ORDER BY e.salary DESC
+                                                                    ) AS Ranking
+                                                                        FROM Employee e
+                                                                            LEFT JOIN Department d
+                                                                                    ON e.departmentId = d.id
+                                                                                    )
+
+                                                                                    SELECT
+                                                                                        Department,
+                                                                                            Employee,
+                                                                                                Salary
+                                                                                                FROM new_table
+                                                                                                WHERE Ranking <= 3;
