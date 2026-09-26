ddd64fa03180025e0c02596821de30799474d4bc
aspapriyan <aspapriyan2007@gmail.com>
2026-09-26 18:50:31 +0530

    Time: 440 ms (53.78%), Space: 273.4 MB (31.61%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,21 +1,13 @@
 class Solution {
         public List<String> findRepeatedDnaSequences(String s) {
-
-                HashSet<String> seen = new HashSet<>();
-                        HashSet<String> result = new HashSet<>();
-
-                                for (int i = 0; i <= s.length() - 10; i++) {
-
-                                            String str = s.substring(i, i + 10);
-
-                                                        if (seen.contains(str)) {
-                                                                        result.add(str);
-                                                                                    } else {
-                                                                                                    seen.add(str);
-                                                                                                                }
-                                                                                                                        }
-
-                                                                                                                                return new ArrayList<>(result);
-                                                                                                                                    }
-                                                                                                                                    
+                Set<String> seen=new HashSet<>();
+                        Set<String>repeated=new HashSet<>();
+                                for(int i=0;i<=s.length()-10;i++){
+                                            String cur=s.substring(i,i+10);
+                                                        if(seen.contains(cur))repeated.add(cur);
+                                                                    else seen.add(cur);
+                                                                            }
+                                                                                    return new ArrayList<>(repeated);
+                                                                                        }
+                                                                                        
 }
